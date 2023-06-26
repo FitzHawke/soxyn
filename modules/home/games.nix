1541ecd14a70f17da2a21ed38e824df3c2cc8e11
FitzHawke <will@fitzhawk.com>
2023-06-26 10:31:39 -0400

    fix: let steam use proton-ge from nix

--- modules/home/games.nix
+++ modules/home/games.nix
@@ -5,12 +5,14 @@
 }: {
   home.packages = with pkgs; [
     inputs.nix-gaming.packages.${pkgs.system}.proton-ge
-    inputs.nix-gaming.packages.${pkgs.system}.wine-ge
     bottles
     gamescope
     mangohud
     ryujinx
     steam
+    wineWowPackages.wayland
     yuzu
   ];
+
+  environment.variables.STEAM_EXTRA_COMPAT_TOOLS_PATHS = "${inputs.nix-gaming.packages.${pkgs.system}.proton-ge}";
 }
