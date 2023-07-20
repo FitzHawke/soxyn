2d75225c5a66b535bc0c462446ea701d32fecaa9
FitzHawke <will@fitzhawk.com>
2023-07-20 00:10:18 -0400

    bottom: update config

--- modules/home/bottom/default.nix
+++ modules/home/bottom/default.nix
@@ -2,30 +2,41 @@
   # universal aliases
   home.packages = with pkgs; [
     (writeScriptBin "htop" ''exec btm'')
-    # yeah it might brake some stuff
-    # but if your script relies on TOP
-    # then its fucking trash
-    # cope
     (writeScriptBin "top" ''exec btm'')
   ];
+  # can get a list of configurable settings by generating a new config with bottom
+  #   btm -C btm.toml
   programs.bottom = {
     enable = true;
     settings = {
       flags.group_processes = true;
       row = [
         {
-          ratio = 2;
+          ratio = 30;
+          child = [{type = "cpu";}];
+        }
+        {
+          ratio = 40;
           child = [
-            {type = "cpu";}
-            {type = "mem";}
+            {
+              ratio = 4;
+              type = "mem";
+            }
+            {
+              ratio = 3;
+              child = [
+                {type = "temp";}
+                {type = "disk";}
+              ];
+            }
           ];
         }
         {
-          ratio = 3;
+          ratio = 30;
           child = [
+            {type = "net";}
             {
               type = "proc";
-              ratio = 1;
               default = true;
             }
           ];
