b808b34bc46bb90bf58c34a4f4ac25159b0f9519
FitzHawke <will@fitzhawk.com>
2023-07-23 23:43:39 -0400

    style: organize user packages

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -1,34 +1,39 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    anki-bin
-    anytype
-    bandwhich
-    bc
-    blueberry
-    catimg
-    cached-nix-shell
-    chromium
-    element-desktop
-    fd
-    ffmpeg
-    foliate
-    gimp
-    glow
-    grex
-    hyperfine
-    imagemagick
-    jellyfin-media-player
-    jq
-    lm_sensors
-    newsflash
-    nmap
-    obsidian
-    quodlibet-full
-    rsync
-    sd
-    todo
-    unzip
-    xh
-    yt-dlp
+    # gui
+    anki-bin # flashcards
+    blueberry # bluetooth gui
+    chromium # backup browser
+    element-desktop # matrix chat app
+    foliate # ebook reader gui
+    gimp # image editing
+    jellyfin-media-player # gui for connecting with jellyfin servers
+    jq # cli json parsing
+    newsflash # RSS reader
+    obsidian # markdown knowledge base
+    quodlibet-full # gui music
+
+    # cli
+    bc # calculator
+    fd # simpler syntax for find
+    catimg # cat for imgs
+    ffmpeg # media conversions
+    glow # render markdown in cli
+    grex # generate regex expressions
+    hyperfine # benchmarking tool
+    imagemagick # image conversion tools
+    nmap # network scanning
+    rsync # folder syncing
+    sd # sed alt in rust with easy syntax
+    unzip # archive manager
+    xh # http requests
+    yt-dlp # youtube
+
+    # other
+    cached-nix-shell # faster nix shells
+    lm_sensors # hardware sensor readouts
+
+    # testing
+    anytype # cloud-synced organizer `everything app`
   ];
 }
