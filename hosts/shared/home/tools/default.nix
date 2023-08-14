1cab565b1c3ff64c09b83cbd88719a7526375ce3
FitzHawke <will@fitzhawk.com>
2023-08-14 01:26:09 -0400

    xdg: add back mimetypes

--- hosts/shared/home/tools/default.nix
+++ hosts/shared/home/tools/default.nix
@@ -3,36 +3,41 @@
   config,
   ...
 }: 
-# let
-#   browser = ["firefox.desktop"];
+let
+  browser = ["firefox.desktop"];
 
-#   associations = {
-#     "text/html" = browser;
-#     "x-scheme-handler/http" = browser;
-#     "x-scheme-handler/https" = browser;
-#     "x-scheme-handler/ftp" = browser;
-#     "x-scheme-handler/about" = browser;
-#     "x-scheme-handler/unknown" = browser;
-#     "application/x-extension-htm" = browser;
-#     "application/x-extension-html" = browser;
-#     "application/x-extension-shtml" = browser;
-#     "application/xhtml+xml" = browser;
-#     "application/x-extension-xhtml" = browser;
-#     "application/x-extension-xht" = browser;
+  # custom settings for default applicatuions
+  associations = {
+    "text/html" = browser;
+    "x-scheme-handler/http" = browser;
+    "x-scheme-handler/https" = browser;
+    "x-scheme-handler/ftp" = browser;
+    "x-scheme-handler/about" = browser;
+    "x-scheme-handler/unknown" = browser;
+    "application/x-extension-htm" = browser;
+    "application/x-extension-html" = browser;
+    "application/x-extension-shtml" = browser;
+    "application/xhtml+xml" = browser;
+    "application/x-extension-xhtml" = browser;
+    "application/x-extension-xht" = browser;
 
-#     "audio/*" = ["mpv.desktop"];
-#     "video/*" = ["mpv.dekstop"];
-#     "image/*" = ["imv.desktop"];
-#     "inode/directory" = ["org.gnome.Nautilus.desktop"];
-#     "application/json" = browser;
-#     "application/pdf" = ["org.pwmt.zathura.desktop"];
-#     "x-scheme-handler/spotify" = ["spotify.desktop"];
-#     "x-scheme-handler/discord" = ["webcord.desktop"];
-#     "x-scheme-handler/element" = ["element-desktop.desktop"];
-#     "x-scheme-handler/anytype" = ["anytype.desktop"];
-#     "x-scheme-handler/steam" = ["steam.desktop"];
-#   };
-# in 
+    # /* doesn't work for all associations. TODO list out audio/video/image/text types manually
+    "audio/*" = ["mpv.desktop"];
+    "video/*" = ["mpv.dekstop"];
+    "image/*" = ["imv.desktop"];
+    "text/*" = ["org.gnome.TextEditor.desktop"];
+    "text/plain" = ["org.gnome.TextEditor.desktop"];
+    "inode/directory" = ["org.gnome.Nautilus.desktop"];
+    "application/json" = browser;
+    "application/pdf" = ["org.pwmt.zathura.desktop"];
+    "x-scheme-handler/spotify" = ["spotify.desktop"];
+    "x-scheme-handler/discord" = ["webcord.desktop"];
+    "x-scheme-handler/element" = ["element-desktop.desktop"];
+    "x-scheme-handler/chrome" = ["chromium-browser.desktop"];
+    "x-scheme-handler/anytype" = ["anytype.desktop"];
+    "x-scheme-handler/steam" = ["steam.desktop"];
+  };
+in 
 {
   services = {
     udiskie.enable = true;
@@ -89,7 +94,7 @@
       publicShare = "${config.home.homeDirectory}/other";
       templates = "${config.home.homeDirectory}/other";
     };
-    # mimeApps.enable = true;
-    # mimeApps.defaultApplications = associations;
+    mimeApps.enable = true;
+    mimeApps.defaultApplications = associations;
   };
 }
