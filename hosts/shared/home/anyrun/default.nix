c65ddc2a23aa2823d7b50b0284a4580dba5fdbce
FitzHawke <will@fitzhawk.com>
2023-08-24 22:06:30 -0400

    anyrun: update styles

--- hosts/shared/home/anyrun/default.nix
+++ hosts/shared/home/anyrun/default.nix
@@ -1,20 +1,19 @@
 {
   inputs,
   pkgs,
+  osConfig,
   ...
 }: {
   imports = [inputs.anyrun.homeManagerModules.default];
   programs.anyrun = {
     enable = true;
-    package = inputs.anyrun.packages.${pkgs.system}.anyrun;
     config = {
       width.fraction = 0.3;
       x.fraction = 0.5;
       y.absolute = 15;
-      ignoreExclusiveZones = false; # default
-      layer = "overlay"; # default
       hidePluginInfo = true;
       closeOnClick = true;
+
       plugins = with inputs.anyrun.packages.${pkgs.system}; [
         applications
         randr
@@ -22,40 +21,70 @@
         rink
         symbols
         kidex
+        inputs.anyrun-nixos-options.packages.${pkgs.system}.default
       ];
     };
-    extraCss = ''
+extraCss = ''
       * {
-          transition: 200ms ease-out;
-          font-family: Lexend;
-          font-size: 1.1rem;
-        }
-        #window,
-        #match,
-        #entry,
-        #plugin,
-        #main {
-          background: transparent;
-        }
-        #match:selected {
-          background: rgba(203, 166, 247, 0.7);
-        }
-        #match {
-          padding: 3px;
-          border-radius: 16px;
-        }
-        #entry {
-          border-radius: 16px;
-        }
-        box#main {
-          background: rgba(30, 30, 46, 0.7);
-          border: 1px solid #28283d;
-          border-radius: 24px;
-          padding: 8px;
-        }
-        row:first-child {
-          margin-top: 6px;
-        }
+        all: unset;
+        font-family: Lexend;
+        font-size: 1.3rem;
+      }
+
+      #window,
+      #match,
+      #entry,
+      #plugin,
+      #main { background: transparent; }
+
+      #match.activatable {
+        border-radius: 16px;
+        padding: .3rem .9rem;
+        margin-top: .01rem;
+      }
+      #match.activatable:first-child { margin-top: .7rem; }
+      #match.activatable:last-child { margin-bottom: .6rem; }
+
+      #plugin:hover #match.activatable {
+        border-radius: 10px;
+        padding: .3rem;
+        margin-top: .01rem;
+        margin-bottom: 0;
+      }
+
+      #match:selected, #match:hover, #plugin:hover {
+        background: rgba(255, 255, 255, .1);
+      }
+
+      #entry {
+        background: rgba(255,255,255,.05);
+        border: 1px solid rgba(255,255,255,.1);
+        border-radius: 16px;
+        margin: .3rem;
+        padding: .3rem 1rem;
+      }
+
+      list > #plugin {
+        border-radius: 16px;
+        margin: 0 .3rem;
+      }
+      list > #plugin:first-child { margin-top: .3rem; }
+      list > #plugin:last-child { margin-bottom: .3rem; }
+      list > #plugin:hover { padding: .6rem; }
+
+      box#main {
+        background: rgba(0, 0, 0, .5);
+        box-shadow: inset 0 0 0 1px rgba(255, 255, 255, .1), 0 0 0 1px rgba(0, 0, 0, .5);
+        border-radius: 24px;
+        padding: .3rem;
+      }
+    '';
+
+    extraConfigFiles."nixos-options.ron".text = ''
+      Config(
+        options_path: "${osConfig.system.build.manual.optionsJSON}/share/doc/nixos/options.json",
+        max_entries: 5,
+      )
     '';
   };
 }
