ea15980345887291eb3fe12200933f35b0a187eb
FitzHawke <will@fitzhawk.com>
2024-06-22 22:13:47 -0400

    games: switch to lutris and add nix dotnet for tmodloader

--- hosts/shared/home/games.nix
+++ hosts/shared/home/games.nix
@@ -1,9 +1,15 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    bottles
     gamescope
+    lutris
     mangohud
     ryujinx
+    winetricks
     wineWowPackages.wayland
   ];
+
+  # Terraria tmodloader dotnet install
+  # Need to change version in tmodloader.runtime.config to match dotnet core version
+  # can be found at [dotnet root]/shared/Microsoft.NETCore.app/8.0.6
+  xdg.dataFile."Steam/steamapps/common/tModLoader/dotnet".source = pkgs.dotnet-sdk_8;
 }
