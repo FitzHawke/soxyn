6848fb36ccfbf6d3836dedb535f85f0b59a8956d
FitzHawke <will@fitzhawk.com>
2024-11-11 13:20:17 -0500

    games: add blurb for impure factorio
    currently disabled, impure for lazy secret management

--- hosts/shared/home/games.nix
+++ hosts/shared/home/games.nix
@@ -1,4 +1,20 @@
-{pkgs, ...}: {
+{pkgs,  ...}: {
+
+  # imports = [/home/will/workspaces/notSoxyn/factorio.nix];
+
+  # Lazy secrets management. Needed at buildtime and I dont know how to do that with agenix...
+  # requires --impure flag to build
+
+  ####### Imported file contents
+  # {pkgs, ...}: {
+  #   home.packages = with pkgs; [
+  #     (factorio-space-age.override {
+  #       username = "Kangaroo";
+  #       token = "aBunch0fNumb3rsAndLetters02541";
+  #     })
+  #   ];
+  # }
+
   home.packages = with pkgs; [
     gamescope
     lutris
