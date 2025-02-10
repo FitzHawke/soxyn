5bb5844aee93350c17368b2a8dd41730f83af3ba
FitzHawke <will@fitzhawk.com>
2025-02-10 12:06:50 -0500

    hypridle: use module for config

--- hosts/shared/home/hypr/hypridle.nix
+++ hosts/shared/home/hypr/hypridle.nix
@@ -3,49 +3,36 @@
   lib,
   ...
 }: let
-  suspendScript = pkgs.writeShellScript "suspend-script" ''
-    ${pkgs.pipewire}/bin/pw-cli i all | ${pkgs.ripgrep}/bin/rg running
-    # only suspend if audio isn't running
-    if [ $? == 1 ]; then
-      ${pkgs.systemd}/bin/systemctl suspend
-    fi
-  '';
+  locktime = 600;
+  lock = "${lib.getExe pkgs.hyprlock}";
+  sleeptime = 900;
+  suspend = "${pkgs.systemd}/bin/systemctl suspend";
 in {
-  home.packages = [pkgs.hypridle];
+  services.hypridle = {
+    enable = true;
 
-  xdg.configFile."hypr/hypridle.conf".text = ''
-    general {
-        lock_cmd = ${lib.getExe pkgs.hyprlock}
-        before_sleep_cmd = ${pkgs.systemd}/bin/loginctl lock-session
-    }
+    settings = {
+      general = {
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+        ignore_dbus_inhibit = false;
+        lock_cmd = lock;
+      };
 
-    # turn off screen after 5 minutes
-    # disabled for testing suspend functionality
-    # listener {
-    #     timeout = 300
-    #     on-timeout = hyprctl dispatch dpms off
-    #     on-resume = hyprctl dispatch dpms on
-    # }
-
-    # suspend system after 15 minutes
-    listener {
-        timeout = 900
-        on-timeout = ${suspendScript.outPath}
-    }
-  '';
-
-  systemd.user.services.hypridle = {
-    Unit = {
-      Description = "Hypridle";
-      After = ["graphical-session.target"];
+      listener = [
+        {
+          timeout = locktime;
+          on-timeout = lock;
+        }
+        {
+          timeout = sleeptime;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+        {
+          timeout = sleeptime + 10;
+          on-timeout = suspend;
+        }
+      ];
     };
-
-    Service = {
-      ExecStart = "${lib.getExe pkgs.hypridle}";
-      Restart = "always";
-      RestartSec = "10";
-    };
-
-    Install.WantedBy = ["default.target"];
   };
 }
