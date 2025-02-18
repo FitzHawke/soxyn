450d6df8a87b7435daad73f4a98ea1d79a92f68f
FitzHawke <will@fitzhawk.com>
2025-02-17 21:24:42 -0500

    astal: add systray functionality

--- hosts/shared/home/astal/config/widgets/bar/buttons/SysTray.tsx
+++ hosts/shared/home/astal/config/widgets/bar/buttons/SysTray.tsx
@@ -1,3 +1,19 @@
+import Tray from "gi://AstalTray";
+import { bind } from "astal";
+
 export const SysTray = () => {
-  return <label label={" tray "} />;
+  const tray = Tray.get_default();
+
+  const SysTrayItem = (item: Tray.TrayItem) => (
+    <menubutton
+      cssClasses={["tray-item"]}
+      tooltipMarkup={bind(item, "tooltipMarkup")}
+      menuModel={bind(item, "menuModel")}
+    >
+      <image gicon={bind(item, "gicon")} />
+    </menubutton>
+  );
+  return (
+    <box children={bind(tray, "items").as((items) => items.map(SysTrayItem))} />
+  );
 };
