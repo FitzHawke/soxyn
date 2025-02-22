4168a7074d77530b82c07a2b458343ad12151bb0
FitzHawke <will@fitzhawk.com>
2025-02-22 10:21:56 -0500

    astal(notifs): add notification indicator on bar

--- hosts/shared/home/astal/config/widgets/bar/buttons/Notifications.tsx
+++ hosts/shared/home/astal/config/widgets/bar/buttons/Notifications.tsx
@@ -1,3 +1,23 @@
+import Notifd from "gi://AstalNotifd";
+import { App } from "astal/gtk4";
+import { BarButton } from "../BarButton";
+import { bind } from "astal";
+
 export const Notifications = () => {
-  return <label label={" notifications "} />;
+  const notifd = Notifd.get_default();
+  const notifs = bind(notifd, "notifications");
+  const windowName = "datemenu";
+  return (
+    <BarButton
+      window={windowName}
+      cssClasses={["messages"]}
+      onClicked={() => App.toggle_window(windowName)}
+      visible={notifs.as((n) => n.length > 0)}
+    >
+      <box>
+        <image iconName={"view-app-grid-symbolic"} />
+        <label label={"Apps"} />
+      </box>
+    </BarButton>
+  );
 };
