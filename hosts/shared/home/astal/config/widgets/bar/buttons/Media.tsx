250fd38df92b2b3034fcb51a159dea456338a36f
FitzHawke <will@fitzhawk.com>
2025-02-24 15:37:39 -0500

    astal(media): rework bindings and variables on bar
    adds ability to switch focused player with middle click

--- hosts/shared/home/astal/config/widgets/bar/buttons/Media.tsx
+++ hosts/shared/home/astal/config/widgets/bar/buttons/Media.tsx
@@ -6,59 +6,68 @@ import { BarButton } from "../BarButton";
 export const Media = () => {
   const mpris = Mpris.get_default();
 
-  const mplayer = bind(mpris, "players").as((ps) => {
-    const player = ps[0];
-    if (!player) return <box />;
+  const players = bind(mpris, "players");
+  const playerNum = Variable(0);
 
-    const lbl = (
-      <label
-        label={bind(player, "metadata").as(
-          () => `${player.artist} - ${player.title}`,
-        )}
-      />
-    );
+  const nextPlayer = () => {
+    playerNum.set((playerNum.get() + 1) % players.get().length);
+  };
+  const curPlayer = Variable.derive(
+    [players, playerNum],
+    (players: Mpris.Player[], num: number) => players[num],
+  );
 
-    const ico = <image iconName={`${player.entry}-symbolic`} />;
+  const reveal = Variable(false);
 
-    const reveal = Variable(false);
+  const lbl = (p: Mpris.Player) => (
+    <label label={bind(p, "metadata").as(() => `${p.artist} - ${p.title}`)} />
+  );
 
-    const rev = (
-      <revealer
-        transitionType={Gtk.RevealerTransitionType.SLIDE_RIGHT}
-        revealChild={bind(reveal)}
-        setup={(self) => {
-          let current = "";
-          hook(self, player, "notify", () => {
-            if (current === player.title) return;
+  const ico = (p: Mpris.Player) => <image iconName={`${p.entry}-symbolic`} />;
 
-            current = player.title;
-            reveal.set(true);
-            timeout(3000, () => reveal.set(false));
-          });
-        }}
-      >
-        {lbl}
-      </revealer>
-    );
+  const rev = (p: Mpris.Player) => (
+    <revealer
+      transitionType={Gtk.RevealerTransitionType.SLIDE_RIGHT}
+      revealChild={bind(reveal)}
+      setup={(self) => {
+        let current = "";
+        hook(self, p, "notify::title", () => {
+          if (current === p.title) return;
 
-    return (
-      <BarButton
-        cssClasses={["media"]}
-        onClicked={() => player.play_pause()}
-        onScroll={(_, _dx, dy) => {
-          if (dy > 0) player.next();
-          else if (dy < 0) player.previous();
-        }}
-        onHoverEnter={() => reveal.set(true)}
-        onHoverLeave={() => reveal.set(false)}
-      >
-        <box>
-          {ico}
-          {rev}
-        </box>
-      </BarButton>
-    );
-  });
+          current = p.title;
+          reveal.set(true);
+          timeout(3000, () => reveal.set(false));
+        });
+      }}
+    >
+      {lbl(p)}
+    </revealer>
+  );
 
-  return mplayer;
+  return (
+    <BarButton
+      cssClasses={["media"]}
+      onButtonReleased={(_, ev) => {
+        ev.get_button() === 2 ? nextPlayer() : curPlayer.get().play_pause();
+      }}
+      onScroll={(_, _dx, dy) => {
+        if (dy > 0) curPlayer.get().next();
+        else if (dy < 0) curPlayer.get().previous();
+      }}
+      onHoverEnter={() => reveal.set(true)}
+      onHoverLeave={() => reveal.set(false)}
+      onDestroy={() => curPlayer.drop()}
+      visible={bind(players).as((p) => p.length > 0)}
+    >
+      {bind(curPlayer).as((cur) => {
+        if (!cur) return <box />;
+        return (
+          <box>
+            {ico(cur)}
+            {rev(cur)}
+          </box>
+        );
+      })}
+    </BarButton>
+  );
 };
